4f0baa902017fff6f1c8bc061b2bc6e1969a2c5b
GravlLift <jason.gravell@gmail.com>
2025-12-03 17:36:29 -0500

    Split 429s from network errors

--- src/request-policy.ts
+++ src/request-policy.ts
@@ -1,48 +1,65 @@
-import { DelegateBackoff, handleType, retry } from 'cockatiel';
+import {
+  DelegateBackoff,
+  handleType,
+  handleWhen,
+  retry,
+  wrap,
+} from 'cockatiel';
 import { isRequestError } from './error-helpers';
 
-export const requestPolicy = retry(
-  handleType(
-    TypeError,
-    (err) =>
-      err.message === 'NetworkError when attempting to fetch resource.' ||
-      err.message === 'Failed to fetch' ||
-      err.message === 'Load failed'
-  ).orWhen(
-    (err) =>
-      isRequestError(err) &&
-      (err.response.status >= 500 ||
-        err.response.status === 401 ||
-        err.response.status === 0 ||
-        err.response.status === 429)
+export const requestPolicy = wrap(
+  retry(
+    handleWhen((err) => isRequestError(err) && err.response.status === 429),
+    {
+      backoff: new DelegateBackoff((context) => {
+        if (
+          'error' in context.result &&
+          isRequestError(context.result.error) &&
+          context.result.error.response.status === 429
+        ) {
+          const retryAfter =
+            context.result.error.response.headers.get('Retry-After');
+          if (retryAfter) {
+            const retryAfterSeconds = parseInt(retryAfter, 10);
+            if (!isNaN(retryAfterSeconds)) {
+              return retryAfterSeconds * 1000;
+            }
+          }
+        }
+
+        return 0;
+      }),
+    }
   ),
-  {
-    maxAttempts: 3,
-    backoff: new DelegateBackoff((context) => {
-      if ('error' in context.result) {
-        if (context.result.error instanceof TypeError) {
+  retry(
+    handleType(
+      TypeError,
+      (err) =>
+        err.message === 'NetworkError when attempting to fetch resource.' ||
+        err.message === 'Failed to fetch' ||
+        err.message === 'Load failed'
+    ).orWhen(
+      (err) =>
+        isRequestError(err) &&
+        (err.response.status >= 500 ||
+          err.response.status === 401 ||
+          err.response.status === 0)
+    ),
+    {
+      maxAttempts: 3,
+      backoff: new DelegateBackoff((context) => {
+        if (
+          'error' in context.result &&
+          (context.result.error instanceof TypeError ||
+            (isRequestError(context.result.error) &&
+              context.result.error.response.status === 0))
+        ) {
           // Add a little delay if the request failed due to a network error
           return 500;
         }
 
-        if (isRequestError(context.result.error)) {
-          if (context.result.error.response.status === 0) {
-            // Add a little delay if the request failed due to a network error
-            return 500;
-          } else if (context.result.error.response.status === 429) {
-            const retryAfter =
-              context.result.error.response.headers.get('Retry-After');
-            if (retryAfter) {
-              const retryAfterSeconds = parseInt(retryAfter, 10);
-              if (!isNaN(retryAfterSeconds)) {
-                return retryAfterSeconds * 1000;
-              }
-            }
-          }
-        }
-      }
-
-      return 0;
-    }),
-  }
+        return 0;
+      }),
+    }
+  )
 );
